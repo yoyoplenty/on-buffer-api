96b725b92415d9b85a8e28b69574e732b2733cfb
Adebiyi Blessing <yoyoplenty@gmail.com>
2026-06-24 09:29:57 +0100

    Auth code update

--- src/app/user/seeder/data.ts
+++ src/app/user/seeder/data.ts
@@ -5,7 +5,7 @@ import { OnboardingStep } from '../types/user.interface';
 export const users = [
   {
     role: 'super-admin',
-    phone: '9000000001',
+    phone: '2349000000001',
     pin: '1234',
     phoneVerified: true,
     status: UserStatus.ACTIVE,
@@ -13,7 +13,7 @@ export const users = [
   },
   {
     role: 'admin',
-    phone: '9000000002',
+    phone: '2349000000002',
     pin: '2345',
     phoneVerified: true,
     status: UserStatus.ACTIVE,
@@ -21,7 +21,7 @@ export const users = [
   },
   {
     role: 'user',
-    phone: '9000000002',
+    phone: '2349000000002',
     pin: '3456',
     phoneVerified: true,
     status: UserStatus.ACTIVE,
